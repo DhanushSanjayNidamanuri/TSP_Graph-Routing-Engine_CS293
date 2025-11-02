37a6981ad274d153631515b5c356f20ce0161920
Dhanush Sanjay Nidamanuri <dhanushsanjayn@gmail.com>
2025-11-02 19:20:15 +0530

    Added isValid to nodes

--- Phase-1/ShortestPath.cpp
+++ Phase-1/ShortestPath.cpp
@@ -1,5 +1,5 @@
 #include<ShortestPath.hpp>
-ShoretestPath_Result ShortestPath::findShortestPath(const Graph& graph, int id, int source, int target,
+ShoretestPath_Result ShortestPath::findShortestPath(Graph& graph, int id, int source, int target,
      const std::string& mode, const std::vector<int>& forbidden_nodes,
         const std::vector<std::string>& forbidden_road_types){
             if(mode=="time"){
