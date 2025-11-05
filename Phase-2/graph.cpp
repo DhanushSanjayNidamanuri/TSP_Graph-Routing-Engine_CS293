ea00a60f0c37952ace796e324c0b0c8f98137839
Dhanush Sanjay Nidamanuri <dhanushsanjayn@gmail.com>
2025-11-05 19:43:32 +0530

    Made names for classes

---
+++ Phase-2/graph.cpp
@@ -0,0 +1,85 @@
+#include "graph.hpp"
+class K_Exact;
+class K_Heuristic;
+class Approx_Shortest;
+void Graph::addNode(const Node& node) {
+    node_list.push_back(node);
+    node_count++;
+    adjacency_list.push_back(std::unordered_map<int,Edge> ());
+}
+
+void Graph::addEdge(const Edge& edge) {
+    edge_list.insert({edge.id,edge});
+    adjacency_list[edge.u][edge.id]=edge;
+    if(!edge.oneway) adjacency_list[edge.v][edge.id]=edge;
+}
+
+bool Graph::removeEdge(int id) {
+    if(edge_list.find(id)==edge_list.end()) return false;
+    Edge edge=edge_list[id];
+    adjacency_list[edge.u][id].isOpen=false;
+    if(!edge.oneway) adjacency_list[edge.v][id].isOpen=false;
+    edge_list[id].isOpen=false;
+    return true;
+}
+
+bool Graph::modifyEdge(int id, double length, double average_time, std::vector<double> speed_profile) {
+    if(edge_list.find(id)==edge_list.end()) return false;
+    Edge edge=edge_list[id];
+    edge.isOpen=true;
+    edge.length=length;
+    edge.average_time=average_time;
+    edge.speed_profile=speed_profile;
+    edge_list[id]=edge;
+    adjacency_list[edge.u][edge.id]=edge;
+    if(!edge.oneway) adjacency_list[edge.v][edge.id]=edge;
+    return true;
+}
+
+nlohmann::json Graph::query_handler(const nlohmann::json& query){
+    nlohmann::json out;
+    if(query["type"]=="remove_edge"){
+        bool done=removeEdge(query["edge_id"]);
+        out["done"]=done;
+        return out;
+    }
+    else if(query["type"]=="modify_edge"){
+        int length=edge_list[query["edge_id"]].length;
+        if(query["patch"].find("length")!=query["patch"].end()){
+            length=query["patch"]["length"];
+        }
+        std::vector<double> speed_profile=edge_list[query["edge_id"]].speed_profile;
+        if(query["patch"].find("speed_profile")!=query["patch"].end()){
+            length=query["patch"]["speed_profile"];
+        }
+        double average_time=edge_list[query["edge_id"]].average_time;
+        if(query["patch"].find("average_time")!=query["patch"].end()){
+            length=query["patch"]["average_time"];
+        }
+        bool done=modifyEdge(query["edge_id"],length,average_time,speed_profile);
+        out["done"]=done;
+        return out;
+    }
+    else if(query["type"]=="shortest_path"){
+        ShortestPath temp;
+        std::vector<int> forbidden_nodes=query["constraints"]["forbidden_nodes"];
+        std::vector<std::string> forbidden_types=query["constraints"]["forbidden_road_types"];
+        ShortestPath_Result tempout=temp.findShortestPath(*this,query["id"],query["source"],query["target"],query["mode"],forbidden_nodes,forbidden_types);
+        out["id"]=tempout.id;
+        out["possible"]=tempout.possible;
+        out["minimum_time/minimum_distance"]=tempout.min_dist_or_time;
+        out["path"]=tempout.path;
+        return out;
+    }
+    else if(query["type"]=="knn"){
+        KNN temp;
+        Result_KNN tempout=temp.findKNN(*this,query["id"],query["query_point"]["lat"],query["query_point"]["lon"],query["poi"],query["k"],query["metric"]);
+        out["id"]=tempout.id;
+        out["nodes"]=tempout.node_ids;
+        return out;
+    }
+    else{
+        return out;
+    }
+    
+}
---
+++ Phase-2/main.cpp
@@ -0,0 +1,89 @@
+#include <nlohmann/json.hpp>
+#include <iostream>
+#include <fstream>
+#include <chrono>
+/* 
+    Add other includes that you require, only write code wherever indicated
+*/
+#include "graph.hpp"
+
+using json = nlohmann::json;
+
+int main(int argc, char* argv[]) {
+    if (argc != 4) {
+        std::cerr << "Usage: " << argv[0] << " <graph.json> <queries.json> <output.json>" << std::endl;
+        return 1;
+    }
+
+    // Read graph from first file
+    /*
+        Add your graph reading and processing code here
+        Initialize any classes and data structures needed for query processing
+    */
+
+
+    ///////-------> MODIFIED FROM HERE <------------//////
+
+    std::ifstream input_graph_file(argv[1]);
+    if (!input_graph_file.is_open()) {
+        std::cerr << "Failed to open " << argv[1] << std::endl;
+        return 1;
+    }
+    json graph_data;
+    input_graph_file>>graph_data;
+    input_graph_file.close();
+    //reading nodes
+    Graph Graph_internal(graph_data["nodes"].size());
+    for(auto x:graph_data["nodes"]){
+        std::vector<std::string> pois=x["pois"].get<std::vector<std::string>>();
+        Node temp(x["id"],x["lat"],x["lon"],pois);
+        Graph_internal.addNode(temp);
+    }
+
+    for(auto x:graph_data["edges"]){
+        std::vector<double> speed_profile=x["speed_profile"].get<std::vector<double>>();
+        Edge temp(x["id"],x["u"],x["v"],x["length"],x["average_time"],speed_profile,x["oneway"],x["road_type"]);
+        Graph_internal.addEdge(temp);
+    }
+    ////////-------> UPTO HERE <-----------///////////////
+
+    // Read queries from second file
+    std::ifstream queries_file(argv[2]);
+    if (!queries_file.is_open()) {
+        std::cerr << "Failed to open " << argv[2] << std::endl;
+        return 1;
+    }
+    json queries_json;
+    queries_file >> queries_json;
+
+    std::vector<json> results;
+
+    for (const auto& query : queries_json["events"]) {
+        auto start_time = std::chrono::high_resolution_clock::now();
+
+        /*
+            Add your query processing code here
+            Each query should return a json object which should be printed to sample.json
+        */
+
+        // Answer each query replacing the function process_query using 
+        // whatever function or class methods that you have implemented
+        json result = Graph_internal.query_handler(query);
+
+        auto end_time = std::chrono::high_resolution_clock::now();
+        result["processing_time"] = std::chrono::duration<double, std::milli>(end_time - start_time).count();
+        results.push_back(result);
+    }
+
+    std::ofstream output_file(argv[3]);
+    if (!output_file.is_open()) {
+        std::cerr << "Failed to open output.json for writing" << std::endl;
+        return 1;
+    }
+
+    json output = results;
+    output_file << output.dump(4) << std::endl;
+
+    output_file.close();
+    return 0;
+}
