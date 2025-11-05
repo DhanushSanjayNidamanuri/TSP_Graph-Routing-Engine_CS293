a02ea25f76c9532dfa346211af698f9dbd132205
vivek <random@gmail.com>
2025-11-05 21:33:43 +0530

    Declared K_shortest and Approx_shortest

---
+++ Phase-2/K_Shortest.hpp
@@ -0,0 +1,28 @@
+#ifndef KSHORTEST_HPP
+#define KSHORTEST_HPP
+
+#include "graph.hpp"
+#include <vector>
+#include <string>
+#include <queue>
+#include <map>
+#include <algorithm>
+class Graph;
+class Node;
+class Edge;
+
+class KShortestPaths_Result {
+public:
+    int id;
+    std::vector<std::vector<int>,int> paths;
+    KShortestPaths_Result(int id, std::vector<std::vector<int>,int> paths) : id(id), paths(paths) {};
+};
+
+class KShortestPaths {
+public:
+    KShortestPaths_Result findShortest(Graph& graph, int id, int source, int target, int k, std::string mode="distance", int overlap_threshold=0);
+    std::vector<std::vector<int>,int> KShortest(Graph& graph, int source, int target, int k, std::string mode);
+    std::vector<std::vector<int>,int> KShortest_heuristic(Graph& graph, int source, int target, int k, int overlap_threshold);
+};
+
+#endif
